4f9991697ebccde2e18ee7bf5f025ff79f3832a6
Martin Litvaj <kamahl19@gmail.com>
2016-12-14 19:12:11 +0100

    fix few bugs in previous merge

--- client/src/reducers/user.js
+++ client/src/reducers/user.js
@@ -1,9 +1,11 @@
+import { combineReducers } from 'redux';
 import { createReducer } from '@src/redux/reduxHelpers';
 import actionTypes from '@src/redux/actionTypes';
 
 const {
     REQUEST, SUCCESS, FAILURE,
-    UPDATE_USER, DELETE_USER, FETCH_USER, FETCH_USERS,
+    //UPDATE_USER,
+    DELETE_USER, FETCH_USER, FETCH_USERS,
 } = actionTypes;
 
 const initialState = {
@@ -17,31 +19,31 @@ const user = createReducer(initialState.user, {
         [SUCCESS]: (state, { user }) => user,
         [FAILURE]: (state) => null,
     }
-})
+});
 
 const removeUser = (users, user) => {
-    const userId = state.users.map((u) => u.id).indexOf(user.id);
-    
+    const userIdx = users.map((u) => u.id).indexOf(user.id);
+
     return [
-        ...users.slice(0, userId),
-        ...users.slice(userId + 1),
-    ]
-}
+        ...users.slice(0, userIdx),
+        ...users.slice(userIdx + 1),
+    ];
+};
 
 const users = createReducer(initialState.users, {
     [FETCH_USERS]: {
         [REQUEST]: (state) => [],
-        [SUCCESS]: (state, { users }) => users,
+        [SUCCESS]: (state, payload) => payload.users,
         [FAILURE]: (state) => [],
     },
     [DELETE_USER]: {
         [REQUEST]: (state) => state,
-        [SUCCESS]: (state, { user }) => removeUser(state, user),
+        [SUCCESS]: (state, payload) => removeUser(state, payload.user),
         [FAILURE]: (state) => state,
     },
-})
+});
 
 export default combineReducers({
     user,
     users,
-})
+});
