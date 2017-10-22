84e3a6e144f3a2e4e5bb3fbd0ae9290739748bce
Martin Litvaj <martin.litvaj@thinkcreatix.com>
2017-10-22 19:21:46 +0200

    [FE] split auth routes

---
+++ frontend/src/features/auth/routes.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+
+import { NotFound } from '../../app/components';
+import { IsAnonymous, LoginWrapper } from './utils';
+import {
+  ForgottenPasswordContainer,
+  LoginContainer,
+  ResetPasswordContainer,
+  SignUpContainer,
+} from './containers';
+
+export default () => (
+  <Switch>
+    <Route exact path="/auth" render={() => <Redirect to="/auth/login" />} />
+    <Route exact path="/auth/login" component={LoginWrapper(LoginContainer)} />
+    <Route exact path="/auth/sign-up" component={IsAnonymous(SignUpContainer)} />
+    <Route
+      exact
+      path="/auth/forgotten-password"
+      component={IsAnonymous(ForgottenPasswordContainer)}
+    />
+    <Route
+      exact
+      path="/auth/reset-password/:passwordResetToken"
+      component={IsAnonymous(ResetPasswordContainer)}
+    />
+
+    <Route component={NotFound} />
+  </Switch>
+);
