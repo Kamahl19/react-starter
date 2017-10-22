3ce2327f8068051748d8537cd96e853477dd4e55
Martin Litvaj <martin.litvaj@thinkcreatix.com>
2017-10-22 20:28:50 +0200

    [RN] use Reactotron

--- rnmobile/src/index.js
+++ rnmobile/src/index.js
@@ -1,6 +1,7 @@
 import { AppRegistry, Platform } from 'react-native';
 import KeyboardManager from 'react-native-keyboard-manager';
 
+import './ReactotronConfig';
 import Root from './app/containers/Root';
 
 AppRegistry.registerComponent('rnmobile', () => Root);
