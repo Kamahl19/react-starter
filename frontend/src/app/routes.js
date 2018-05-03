29ca70dd0a2794a5c7086bf910a8d55009b023bb
Martin Litvaj <martin.litvaj@thinkcreatix.com>
2018-05-03 11:38:21 +0200

    [FE] simplify demo page

--- frontend/src/app/routes.js
+++ frontend/src/app/routes.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import { IsLoggedIn } from '../features/auth/utils';
-import WelcomeScreen from '../features/welcome/WelcomeScreen';
 import AuthRoutes from '../features/auth/routes';
 import { NotFound } from '../common/components';
 
 export default (
   <Switch>
-    <Route exact path="/" component={WelcomeScreen} />
+    <Route exact path="/" render={() => <h1>Welcome</h1>} />
 
     <Route exact path="/me" component={IsLoggedIn(() => <h1>User Profile</h1>)} />
 
