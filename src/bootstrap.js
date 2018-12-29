990f24346c49a2297db329ead7841287ae423d0c
Martin Litvaj <martin.litvaj@thinkcreatix.com>
2018-12-29 17:36:43 +0100

    upgrade deps

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,5 +3,8 @@
  */
 import 'react-app-polyfill/ie9';
 
-// TODO remove when https://bugs.chromium.org/p/chromium/issues/detail?id=872770 is fixed
+/**
+ * Fix google translate bug
+ * TODO remove when https://bugs.chromium.org/p/chromium/issues/detail?id=872770 is fixed
+ */
 import './packages/react-google-translate-fix';
