eae761b040ee444ff5a6902ddc7cbc4f2acb223b
Miroslav Petrik <miroslavpetrik@outlook.com>
2019-02-02 22:40:38 +0100

    Cleanup common components imports (#77)
    fix https://github.com/Kamahl19/react-starter/issues/20

--- src/common/components/Header/index.js
+++ src/common/components/Header/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 
-import Layout from '../Layout';
-import Logo from '../Logo';
-import Navbar from '../Navbar';
+import { Layout, Logo, Navbar } from '../';
 
 const Header = ({ children, history, location }) => (
   <Layout.Header>
