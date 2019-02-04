fcd421ca81a746294da2b12cd1ce07b49a427507
Miroslav Petrik <miroslav.petrik@thinkcreatix.com>
2019-02-04 19:42:15 +0100

    Fix

--- src/features/auth/components/PageLayout/index.js
+++ src/features/auth/components/PageLayout/index.js
@@ -3,23 +3,45 @@ import PropTypes from 'prop-types';
 import { Trans } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
-import { Layout, Header, Footer, Menu } from '../../../../common/components';
+import { Footer, Header, Icon, Layout, Menu, Popover } from '../../../../common/components';
 
 const PageLayout = ({ children }) => (
   <Layout>
     <Header>
-      {() => [
-        <Menu.Item key="signup">
-          <Link to="/sign-up">
-            <Trans i18nKey="nav.signup">Sign Up</Trans>
-          </Link>
-        </Menu.Item>,
-        <Menu.Item key="login">
-          <Link to="/login">
-            <Trans i18nKey="nav.login">Log In</Trans>
-          </Link>
-        </Menu.Item>,
-      ]}
+      {({ activePath, hideMenu, showMenu, isMenuVisible, isMobile }) => {
+        const menu = (
+          <Menu
+            mode={isMobile ? 'inline' : 'horizontal'}
+            theme={isMobile ? undefined : 'dark'}
+            selectedKeys={[activePath]}
+          >
+            <Menu.Item>
+              <Link to="/sign-up">
+                <Trans i18nKey="nav.signup">Sign Up</Trans>
+              </Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link to="/login">
+                <Trans i18nKey="nav.login">Log In</Trans>
+              </Link>
+            </Menu.Item>
+          </Menu>
+        );
+
+        return isMobile ? (
+          <Popover
+            content={menu}
+            onVisibleChange={visible => (visible ? showMenu() : hideMenu())}
+            title={<Icon type="close" onClick={hideMenu} />}
+            trigger="click"
+            visible={isMenuVisible}
+          >
+            <Icon type="bars" />
+          </Popover>
+        ) : (
+          menu
+        );
+      }}
     </Header>
     <Layout.Content>{children}</Layout.Content>
     <Footer />
