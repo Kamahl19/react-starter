7d68f636e820552f809a9d8c1cfb1060b7a69cc9
Miroslav Petrik <miroslav.petrik@thinkcreatix.com>
2019-02-04 21:35:04 +0100

    drop common/header

--- src/features/auth/components/PageLayout/index.js
+++ src/features/auth/components/PageLayout/index.js
@@ -3,46 +3,49 @@ import PropTypes from 'prop-types';
 import { Trans } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
-import { Footer, Header, Icon, Layout, Menu, Popover } from '../../../../common/components';
+import ResponsiveNavigation from '../../../../packages/responsive-navigation';
+import { Footer, Icon, Layout, Menu, Popover } from '../../../../common/components';
 
 const PageLayout = ({ children }) => (
   <Layout>
-    <Header>
-      {({ activePath, hideNavigation, showNavigation, isNavigationVisible, isMobile }) => {
-        const menu = (
-          <Menu
-            mode={isMobile ? 'inline' : 'horizontal'}
-            theme={isMobile ? undefined : 'dark'}
-            selectedKeys={[activePath]}
-          >
-            <Menu.Item>
-              <Link to="/sign-up">
-                <Trans i18nKey="nav.signup">Sign Up</Trans>
-              </Link>
-            </Menu.Item>
-            <Menu.Item>
-              <Link to="/login">
-                <Trans i18nKey="nav.login">Log In</Trans>
-              </Link>
-            </Menu.Item>
-          </Menu>
-        );
+    <Layout.Header>
+      <ResponsiveNavigation>
+        {({ activePath, hideNavigation, showNavigation, isNavigationVisible, isMobile }) => {
+          const menu = (
+            <Menu
+              mode={isMobile ? 'inline' : 'horizontal'}
+              theme={isMobile ? undefined : 'dark'}
+              selectedKeys={[activePath]}
+            >
+              <Menu.Item>
+                <Link to="/sign-up">
+                  <Trans i18nKey="nav.signup">Sign Up</Trans>
+                </Link>
+              </Menu.Item>
+              <Menu.Item>
+                <Link to="/login">
+                  <Trans i18nKey="nav.login">Log In</Trans>
+                </Link>
+              </Menu.Item>
+            </Menu>
+          );
 
-        return isMobile ? (
-          <Popover
-            content={menu}
-            onVisibleChange={visible => (visible ? showNavigation() : hideNavigation())}
-            title={<Icon type="close" onClick={hideNavigation} />}
-            trigger="click"
-            visible={isNavigationVisible}
-          >
-            <Icon type="bars" />
-          </Popover>
-        ) : (
-          menu
-        );
-      }}
-    </Header>
+          return isMobile ? (
+            <Popover
+              content={menu}
+              onVisibleChange={visible => (visible ? showNavigation() : hideNavigation())}
+              title={<Icon type="close" onClick={hideNavigation} />}
+              trigger="click"
+              visible={isNavigationVisible}
+            >
+              <Icon type="bars" style={{ color: '#fff' }} />
+            </Popover>
+          ) : (
+            menu
+          );
+        }}
+      </ResponsiveNavigation>
+    </Layout.Header>
     <Layout.Content>{children}</Layout.Content>
     <Footer />
   </Layout>
