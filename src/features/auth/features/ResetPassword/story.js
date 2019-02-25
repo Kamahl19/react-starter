dfa821445ca3fce5c183309570ad79ddd5138e5a
Martin Litvaj <kamahl19@gmail.com>
2019-02-25 12:59:08 +0100

    add knobs addon

--- src/features/auth/features/ResetPassword/story.js
+++ src/features/auth/features/ResetPassword/story.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, boolean } from '@storybook/addon-knobs';
 
 import ResetPassword from './view';
 
-storiesOf('auth/ResetPassword', module).add('default', () => (
-  <ResetPassword isLoading={false} onSubmit={action('submit')} />
-));
+storiesOf('auth/ResetPassword', module)
+  .addDecorator(withKnobs)
+  .add('default', () => (
+    <ResetPassword isLoading={boolean('isLoading', false)} onSubmit={action('submit')} />
+  ));
