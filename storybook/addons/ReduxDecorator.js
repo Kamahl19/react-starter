fbacecacbdd3a86215529e0debea42122d06d4bc
Martin Litvaj <kamahl19@gmail.com>
2019-02-25 14:28:22 +0100

    remove unused addons

--- storybook/addons/ReduxDecorator.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
-
-const createReduxDecorator = reducers => story => (
-  <Provider store={createStore(combineReducers(reducers))}>{story()}</Provider>
-);
-
-export default createReduxDecorator;
