fbca5d5d8f58446f103de3ccd95d85bfa009cc16
Martin Litvaj <kamahl19@gmail.com>
2019-06-17 20:47:37 +0200

    fix spinner

--- src/packages/spinner/useSpinner.ts
+++ src/packages/spinner/useSpinner.ts
@@ -1,13 +1,7 @@
-import { useContext } from 'react';
-import { ReactReduxContext } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import { selectIsInProgress, GLOBAL } from './ducks';
 
-export default function useSpinner(id = GLOBAL) {
-  const { store } = useContext(ReactReduxContext);
+const useSpinner = (id = GLOBAL) => useSelector(state => selectIsInProgress(state, id));
 
-  return selectIsInProgress(store.getState(), id);
-}
-
-// TODO useSelector
-// const useSpinner = (id = GLOBAL) => useSelector(state => selectIsInProgress(state, id));
+export default useSpinner;
