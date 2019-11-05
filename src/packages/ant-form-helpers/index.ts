6a35f5170dc81deabff6b7940d8f383b09a2e911
Martin Litvaj <mlitvaj@gohealth.com>
2019-11-06 00:47:19 +0100

    use proper FormComponentProps name

--- src/packages/ant-form-helpers/index.ts
+++ src/packages/ant-form-helpers/index.ts
@@ -1,7 +1,7 @@
-import { FormComponentProps } from 'antd/lib/form';
+import { FormComponentProps as FormProps } from 'antd/lib/form';
 
 export { default as FormItem } from './FormItem';
 export { default as FormScreen } from './FormScreen';
 
 // TODO this can be replaced by simple re-export of `FormComponentProps` when `--isolatedModules` is removed from CRA
-export type FormProps<V = any> = FormComponentProps<V>;
+export type FormComponentProps<V = any> = FormProps<V>;
