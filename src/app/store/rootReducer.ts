104e8ae3589cb4025b3b267831c26021537decb6
Martin Litvaj <mlitvaj@gohealth.com>
2019-11-16 01:27:39 +0100

    #493 fix reseting store

--- src/app/store/rootReducer.ts
+++ src/app/store/rootReducer.ts
@@ -5,17 +5,18 @@ import { isActionOf } from 'typesafe-actions';
 
 import { spinnerReducer as spinner } from 'packages/spinner';
 
+import { RootState } from './';
 import user, { logoutAction } from 'common/services/user';
 
-export default function createRootReducer(history: History) {
+export default function createRootReducer(history: History): typeof rootReducer {
   const rootReducer = combineReducers({
     user,
     spinner,
     router: connectRouter(history),
   });
 
-  return ((state, action) =>
+  return (state, action) =>
     isActionOf(logoutAction, action)
-      ? rootReducer(undefined, action)
-      : rootReducer(state, action)) as typeof rootReducer;
+      ? rootReducer(state ? ({ router: state.router } as RootState) : undefined, action)
+      : rootReducer(state, action);
 }
