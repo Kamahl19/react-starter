f66e290ed1dc522b10600a5877108d66d3b9f102
Martin Litvaj <kamahl19@gmail.com>
2019-12-30 15:13:33 +0100

    add Netlify link to apply for Netlify's free OpenSource Plan

--- src/features/auth/components/AuthLayout/index.tsx
+++ src/features/auth/components/AuthLayout/index.tsx
@@ -28,6 +28,10 @@ const AuthLayout = ({ children }: Props) => {
         </Navbar>
       </Layout.Header>
       <Layout.Content>{children}</Layout.Content>
+      {/* You can remove the Netlify link. It's here only to conform with Netlify's Open Source Plan Policy (https://www.netlify.com/legal/open-source-policy/) */}
+      <Layout.Footer style={{ textAlign: 'center' }}>
+        <a href="https://www.netlify.com">This site is powered by Netlify</a>
+      </Layout.Footer>
     </Layout>
   );
 };
