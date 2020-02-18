243887285b493eef92262ee5e48b6f441fb33cb5
Martin Litvaj <mlitvaj@gohealth.com>
2020-02-18 14:44:21 +0100

    fix test

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,3 +1,18 @@
 import '@testing-library/jest-dom/extend-expect';
 
 import './bootstrap';
+
+// Mock window.matchMedia https://jestjs.io/docs/en/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
