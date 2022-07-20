69f63cddccc269c37d254c9ce5dc0e5a9b92a270
Martin Litvaj <kamahl19@gmail.com>
2022-07-20 13:48:05 +0200

    Fix matchMedia mock for tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -9,19 +9,16 @@ document.title = 'React Starter';
 createFetchMock(vi).enableMocks();
 
 // Mock matchMedia
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: vi.fn().mockImplementation((query: string) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: vi.fn(),
-    removeListener: vi.fn(),
-    addEventListener: vi.fn(),
-    removeEventListener: vi.fn(),
-    dispatchEvent: vi.fn(),
-  })),
-});
+vi.stubGlobal('matchMedia', (query: string) => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addListener: vi.fn(),
+  removeListener: vi.fn(),
+  addEventListener: vi.fn(),
+  removeEventListener: vi.fn(),
+  dispatchEvent: vi.fn(),
+}));
 
 // eslint-disable-next-line import/first
 import 'common/i18next';
