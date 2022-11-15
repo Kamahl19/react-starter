271125afdb39f86972df8172c92563e58964dee0
Martin Litvaj <kamahl19@gmail.com>
2022-11-16 00:39:02 +0100

    Upgrade i18next

--- i18next-parser.config.ts
+++ i18next-parser.config.ts
@@ -1,7 +1,9 @@
-module.exports = {
+const config = {
   locales: ['en'],
   input: ['src/**/*.{ts,tsx}', '!src/**/*.{spec,test}.{ts,tsx}', '!src/**/__tests__/**/*'],
   output: 'public/locales/$LOCALE/$NAMESPACE.json',
   sort: true,
   verbose: true,
 };
+
+export default config;
--- src/i18n.d.ts
+++ src/i18n.d.ts
@@ -1,8 +1,8 @@
-import 'react-i18next';
+import 'i18next';
 
 import type translation from '../public/locales/en/translation.json';
 
-declare module 'react-i18next' {
+declare module 'i18next' {
   // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
   interface CustomTypeOptions {
     resources: {
