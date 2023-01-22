6401e6c97f6b4eca8ef9a8720888f1ced315adeb
Martin Litvaj <kamahl19@gmail.com>
2023-01-22 13:36:46 +0100

    Update dependencies

--- src/i18n/types.d.ts
+++ src/i18n/types.d.ts
@@ -5,7 +5,7 @@ import { type resources } from '.';
 declare module 'i18next' {
   // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
   interface CustomTypeOptions {
-    resources: typeof resources['en'];
+    resources: typeof resources.en;
     returnNull: false;
   }
 }
