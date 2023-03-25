87ff452368f4bde400ca86dd3957a1e8803e0f33
Martin Litvaj <kamahl19@gmail.com>
2023-03-25 19:27:12 +0100

    Update deps

--- src/app/GlobalErrorBoundary.tsx
+++ src/app/GlobalErrorBoundary.tsx
@@ -1,5 +1,5 @@
 import { type ReactNode } from 'react';
-import { ErrorBoundary, type FallbackProps } from 'react-error-boundary';
+import { ErrorBoundary } from 'react-error-boundary';
 
 import { centeredCss } from 'common/styleUtils';
 
@@ -9,11 +9,11 @@ type Props = {
 
 const GlobalErrorBoundary = ({ children }: Props) => (
   <ErrorBoundary
-    FallbackComponent={({ resetErrorBoundary, error }: FallbackProps) => (
+    FallbackComponent={({ resetErrorBoundary, error }) => (
       <div css={centeredCss}>
         <h1>Unexpected Error</h1>
         <p>This is a problem on our side, not yours.</p>
-        <pre>Error: {error.message}</pre>
+        <pre>Error: {error instanceof Error ? error.message : error}</pre>
         <p>
           <button onClick={resetErrorBoundary} type="button">
             Try again
