072ebf332228f4951c7da0ede608ca4754058481
Martin Litvaj <kamahl19@gmail.com>
2023-05-20 15:21:01 +0200

    Fix integration test

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -2,12 +2,6 @@ import { render, screen, userEvent } from 'tests/utils';
 
 import App from './App';
 
-// Preload lazy component to avoid increating timeout
-import 'features/auth';
-import 'features/dashboard';
-import 'features/auth/pages/SignUp';
-import 'features/auth/pages/Login';
-
 const email = 'email@example.com';
 const password = 'password';
 
@@ -17,19 +11,19 @@ test('creates user and logs-in', async () => {
   render(<App />);
 
   // Sign up
-  await click(await screen.findByRole('link', { name: 'auth:menu.signUp' }));
+  await click(await screen.findByRole('link', { name: 'auth:menu.signUp' }, { timeout: 5000 }));
   await type(await screen.findByLabelText('auth:signUp.email'), email);
   await type(await screen.findByLabelText('auth:signUp.password'), password);
   await click(await screen.findByRole('button', { name: 'auth:signUp.submit' }));
 
   // Log in
-  await screen.findByText('auth:signUp.success');
+  await screen.findByText('auth:signUp.success', undefined, { timeout: 5000 });
   await type(await screen.findByLabelText('auth:logIn.email'), email);
   await type(await screen.findByLabelText('auth:logIn.password'), password);
   await click(await screen.findByRole('button', { name: 'auth:logIn.submit' }));
 
   // Log out
-  await click(await screen.findByText(email));
+  await click(await screen.findByText(email, undefined, { timeout: 5000 }));
   await click(await screen.findByText('dashboard:topMenu.logout'));
   expect(await screen.findByRole('link', { name: 'auth:menu.logIn' })).toBeInTheDocument();
 });
