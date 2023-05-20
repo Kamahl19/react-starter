5d8412d0342dcc3b30ccdfb7a22a538f31b6f2e5
Martin Litvaj <kamahl19@gmail.com>
2023-05-20 15:21:37 +0200

    Timeout constant

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -5,25 +5,27 @@ import App from './App';
 const email = 'email@example.com';
 const password = 'password';
 
+const timeout = 5000;
+
 test('creates user and logs-in', async () => {
   const { click, type } = userEvent.setup();
 
   render(<App />);
 
   // Sign up
-  await click(await screen.findByRole('link', { name: 'auth:menu.signUp' }, { timeout: 5000 }));
+  await click(await screen.findByRole('link', { name: 'auth:menu.signUp' }, { timeout }));
   await type(await screen.findByLabelText('auth:signUp.email'), email);
   await type(await screen.findByLabelText('auth:signUp.password'), password);
   await click(await screen.findByRole('button', { name: 'auth:signUp.submit' }));
 
   // Log in
-  await screen.findByText('auth:signUp.success', undefined, { timeout: 5000 });
+  await screen.findByText('auth:signUp.success', undefined, { timeout });
   await type(await screen.findByLabelText('auth:logIn.email'), email);
   await type(await screen.findByLabelText('auth:logIn.password'), password);
   await click(await screen.findByRole('button', { name: 'auth:logIn.submit' }));
 
   // Log out
-  await click(await screen.findByText(email, undefined, { timeout: 5000 }));
+  await click(await screen.findByText(email, undefined, { timeout }));
   await click(await screen.findByText('dashboard:topMenu.logout'));
   expect(await screen.findByRole('link', { name: 'auth:menu.logIn' })).toBeInTheDocument();
 });
