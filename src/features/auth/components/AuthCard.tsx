523987c00d12a423cdc52e7dc245a1b8bfa24ddd
Martin Litvaj <kamahl19@gmail.com>
2023-05-25 10:19:27 +0200

    Update deps

--- src/features/auth/components/AuthCard.tsx
+++ src/features/auth/components/AuthCard.tsx
@@ -3,7 +3,6 @@ import { Card, Typography } from 'antd';
 import { css } from '@emotion/react';
 
 import { createStyles } from 'common/styleUtils';
-import { SpaceVertical } from 'common/components';
 
 type Props = {
   children: ReactNode;
@@ -12,10 +11,8 @@ type Props = {
 
 const AuthCard = ({ children, title }: Props) => (
   <Card css={styles.self}>
-    <SpaceVertical>
-      <Typography.Title level={3}>{title}</Typography.Title>
-      {children}
-    </SpaceVertical>
+    <Typography.Title level={3}>{title}</Typography.Title>
+    {children}
   </Card>
 );
 
